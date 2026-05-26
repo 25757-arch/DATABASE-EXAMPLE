e38117203ba41074d6861610433ff92e6b14de75
25757-arch <25757@burnside.school.nz>
2026-05-26 17:21:34 +1200

    123

--- app.py
+++ app.py
@@ -1,1 +1,40 @@
-print("Helo, World!")
+#docstring- Steve Rodkiss- airb=plane detabse application
+#imports
+import sqlite3
+
+#contants and variables
+DATABASE = "fighters.db"
+
+
+#functions
+def print_all_aircraft():
+    '''print all the aircraft nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"name                           speed    max_g climb range payload")
+    for fighter in results:
+        print(f"{fighter[1]:<30}{fighter[2]:<8}{fighter[3]:<6}{fighter[4]:<6}{fighter[5]:<6}{fighter[6]:<6}")
+    #loop finished here
+    db.close()
+
+
+
+#main code
+while True:
+    user_input = input("\nWhat would you like to do.\n1. Print all aircraft\n7. Exit\n")
+    if user_input == "1":
+        print_all_aircraft()
+    if user_input == "2":
+        pass
+    if user_input == "3":
+        pass
+    if user_input == "7":
+        break
+    else:
+        print("That was not an option\n")
+
+        # 
